b01e6c61788ceacfeb8c793d0ba3f2f0f4abbfed
LeonardoBizzoni <leo2002714@gmail.com>
2024-10-25 08:44:32 +0200

    Dynamic library stuff on Linux

---
+++ OS/Linux/dynlib.cpp
@@ -0,0 +1,30 @@
+#ifndef OS_LINUX_DYNLIB
+#define OS_LINUX_DYNLIB
+
+#include "../../string.hpp"
+
+#include <dlfcn.h>
+
+namespace OS::DynLib {
+inline fn void *open(Base::String8 path) {
+  return dlopen((char *)path.str, RTLD_LAZY);
+}
+
+inline fn void *lookup(void *handle, Base::String8 symbol) {
+  Assert(!dlerror());
+  Assert(handle);
+  void *res = dlsym(handle, (char *)symbol.str);
+  Assert(res);
+  Assert(!dlerror());
+
+  return res;
+}
+
+inline fn void close(void *handle) {
+  Assert(handle);
+  i8 res = dlclose(handle);
+  Assert(!res);
+}
+} // namespace OS::DynLib
+
+#endif
