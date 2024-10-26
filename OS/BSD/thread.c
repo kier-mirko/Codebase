a186072a92910d908e21c2298f81d33ab52d73f6
LeonardoBizzoni <leo2002714@gmail.com>
2024-10-26 12:50:14 +0200

    Switched BSD stuff to C as well

---
+++ OS/BSD/thread.c
@@ -0,0 +1,31 @@
+#ifndef BASE_OS_THREAD
+#define BASE_OS_THREAD
+
+#include "thread.h"
+
+inline fn Thread *threadSpawn(void *(*thread_main)(void *)) {
+  return threadSpawnArgs(thread_main, 0);
+}
+
+fn Thread *threadSpawnArgs(void *(*thread_main)(void *), void *arg_data) {
+  Assert(thread_main);
+
+  Thread *thread;
+  i32 res = pthread_create(&thread, 0, thread_main, arg_data);
+
+  if (!thread || res != 0) {
+    perror("`Base::OS::spawn_thread`");
+  }
+
+  return thread;
+}
+
+inline fn void threadJoin(Thread *tcb) {
+  threadJoinReturn(tcb, 0);
+}
+
+fn void threadJoinReturn(Thread *tcb, void **save_return_value_in) {
+  (void)pthread_join(tcb, save_return_value_in);
+}
+
+#endif
---
+++ OS/BSD/thread.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "../../base.h"
+
+#include <pthread.h>
+#include <stdio.h>
+
+typedef struct pthread Thread;
+
+inline fn Thread *threadSpawn(void *(*thread_main)(void *));
+       fn Thread *threadSpawnArgs(void *(*thread_main)(void *), void *arg_data);
+
+inline fn void threadJoin(Thread *tcb);
+       fn void threadJoinReturn(Thread *tcb, void **save_return_value_in);
